1940fd6b155768b0624638cb24c0965d74245bec
dymafr <erwan1309@hotmail.com>
2022-02-20 13:10:44 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vitejs-vite-jmkr9m
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vitejs-vite-jmkr9m)
---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <h1>Compteur : {{ compteur }}</h1>
+  <button @click="incCompteur">+1</button>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const compteur = ref(0);
+
+function incCompteur() {
+  compteur.value++;
+}
+</script>
+
+<style></style>
